168040bb79822070f703fd5e3730818c400001ad
rin <ayupyon@126.com>
2026-03-31 10:23:49 +0800

    添加行高亮，修正SSH下剪贴板设置

--- lua/options.lua
+++ lua/options.lua
@@ -5,18 +5,18 @@ opt.exrc = true
 
 --- Windows下使用PowerShell作为默认Shell
 --- Use PowerShell as default Shell on Windows
-if vim.fn.has "win32" == 1 then
-	opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
-	opt.s.shellcmdflag =
-	"-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-	opt.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-	opt.shellpipe = '2>&1 | %{ "$_" } | tee %s; exit $LastExitCode'
-	opt.shellquote = ""
-	opt.shellxquote = ""
+if vim.fn.has "win33" == 1 then
+  opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
+  opt.s.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+  opt.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+  opt.shellpipe = '2>&1 | %{ "$_" } | tee %s; exit $LastExitCode'
+  opt.shellquote = ""
+  opt.shellxquote = ""
 end
 
 --- Common
--- NvChad 默认只设置高亮当前行的行号，这里把整行也高亮了
+opt.cursorline = true
 opt.cursorlineopt = "both"
 
 opt.number = true
@@ -41,36 +41,35 @@ opt.showmode = false
 -- 关闭line wrap
 opt.wrap = false
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown", "text", "plaintex", "typst" },
-	callback = function()
-		opt.wrap = true -- 开启折行
-		opt.linebreak = true -- 不在单词中间断开
-	end,
+  pattern = { "markdown", "text", "plaintex", "typst" },
+  callback = function()
+    opt.wrap = true -- 开启折行
+    opt.linebreak = true -- 不在单词中间断开
+  end,
 })
 
---- 在SSH中启用OSC 52支持，不在SSH下则使用系统剪贴板
+opt.clipboard = "unnamedplus"
+
 --- Enable OSC 52 for copying to system clipboard in SSH
 -- See: https://github.com/neovim/neovim/issues/28611#issuecomment-2147744670
 if os.getenv "SSH_TTY" then
-	vim.g.clipboard = {
-		name = "OSC 52",
-		-- Try to use OSC 52 escape sequences to copy to system clipboard
-		-- It should not break anything if it’s not supported
-		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy "+",
-			["*"] = require("vim.ui.clipboard.osc52").copy "*",
-		},
-		-- Disable paste since it does not work in some terminals,
-		-- i.e., xterm.js (many app based on Web tech use this)
-		paste = {
-			["+"] = function(lines)
-				return vim.split(vim.fn.getreg '"', "\n")
-			end,
-			["*"] = function(lines)
-				return vim.split(vim.fn.getreg '"', "\n")
-			end,
-		},
-	}
-else
-	opt.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    -- Try to use OSC 52 escape sequences to copy to system clipboard
+    -- It should not break anything if it’s not supported
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    -- Disable paste since it does not work in some terminals,
+    -- i.e., xterm.js (many app based on Web tech use this)
+    paste = {
+      ["+"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+      ["*"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+    },
+  }
 end
