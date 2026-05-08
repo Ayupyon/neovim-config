d8f07ff9361e0456724609b1268f73ad05bf370d
rin <ayupyon@126.com>
2026-05-08 17:59:57 +0800

    add vim.g.auto_format option to control format of conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,11 +12,18 @@ return {
         -- Conform will run the first available formatter
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 2000,
-        lsp_format = "fallback",
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return {
+          -- These options will be passed to conform.format()
+          timeout_ms = 2000,
+          lsp_format = "fallback",
+        }
+      end
     },
   },
 }
