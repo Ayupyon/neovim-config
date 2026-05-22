776b4d8190ab8e2c42dd25eca64a1c35d8b6a77a
rin <ayupyon@126.com>
2026-05-22 19:32:58 +0800

    change python default lsp from pyright to basedpyright

--- lua/configs/lsp/python.lua
+++ lua/configs/lsp/python.lua
@@ -1,13 +1,4 @@
 return {
-  name = "pyright",
-  config = {
-    settings = {
-      python = {
-        -- Enable semantic tokens (pyright supports this by default)
-        analysis = {
-          typeCheckingMode = "basic",
-        },
-      },
-    },
-  },
+  name = "basedpyright",
+  config = {},
 }
