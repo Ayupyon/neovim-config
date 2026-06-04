b4721a207a1a4586843d1ddde9fbc79453683f1e
rin <ayupyon@126.com>
2026-06-04 13:15:32 +0800

    feat: add TSInstallCommon command

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,21 @@
+local common_parsers = {
+  "cpp",
+  "go",
+  "python",
+  "bash",
+  "lua",
+  "rust",
+  "javascript",
+  "typescript",
+  "html",
+  "css",
+  "json",
+  "yaml",
+  "typst",
+  "markdown",
+  "toml",
+}
+
 ---@type LazySpec
 return {
   {
@@ -6,25 +24,19 @@ return {
     build = ":TSUpdate",
     config = function()
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          'cpp',
-          'go',
-          'python',
-          'bash',
-          'lua',
-          'rust',
-          'javascript',
-          'typescript',
-          'html',
-          'css',
-          'json',
-          'yaml',
-          'typst',
-        },
+        pattern = common_parsers,
         callback = function()
           vim.treesitter.start()
         end,
       })
-    end
+
+      vim.api.nvim_create_user_command("TSInstallCommon", function()
+        -- Print a message so you know it started
+        vim.notify("Installing common Tree-sitter parsers...", vim.log.levels.INFO)
+
+        -- Pass the list of parsers to the TSInstall command
+        vim.cmd("TSInstall " .. table.concat(common_parsers, " "))
+      end, { desc = "install common treesitters" })
+    end,
   },
 }
